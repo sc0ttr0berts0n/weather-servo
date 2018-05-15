be84de406cbc2568a923484482231264fbaedede
cyko28 <scott.t.robertson@gmail.com>
2018-05-15 19:39:09 -0400

    using personal api

--- arduino/servo/servo.ino
+++ arduino/servo/servo.ino
@@ -29,8 +29,8 @@ void loop()
 
         HTTPClient http;
 
-        http.begin("https://api.darksky.net/forecast/2b11c9e517f8526db8af5ba89d7a6e89/37.8267,-122.4233&exclude=currently,minutely,daily,alerts,flags"); //Specify the URL
-        int httpCode = http.GET();                                                                                                                       //Make the request
+        http.begin("https://weather-servo.herokuapp.com/ds"); //Specify the URL
+        int httpCode = http.GET();                            //Make the request
 
         if (httpCode > 0)
         { //Check for the returning code
