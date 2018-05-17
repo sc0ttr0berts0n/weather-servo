dee62ae6b55dc221dbe2e61d971bbb86cd98105d
cyko28 <scott.t.robertson@gmail.com>
2018-05-16 21:29:44 -0400

    added servo portion

--- arduino/servo/servo.ino
+++ arduino/servo/servo.ino
@@ -1,15 +1,27 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include "Credentials.h"
+#include <ESP32_Servo.h>
 
+Servo servo;       // initialize the servo object
+int servoPin = 18; // use pin 18
+
+// Init Wifi Vars
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWD;
+
+// Init var to hold weather type string
+String weatherType;
 
 void setup()
 {
 
     Serial.begin(115200);
     delay(4000);
+
+    servo.attach(servoPin, 500, 2500); // attach the servo
+    delay(10);
+
     WiFi.begin(ssid, password);
 
     while (WiFi.status() != WL_CONNECTED)
@@ -29,24 +41,41 @@ void loop()
 
         HTTPClient http;
 
-        http.begin("https://weather-servo.herokuapp.com/ds"); //Specify the URL
-        int httpCode = http.GET();                            //Make the request
+        http.begin("https://weather-servo.herokuapp.com/ds?key=" + String(SECRET_KEY)); //Specify the URL
+        int httpCode = http.GET();                                                      //Make the request
 
         if (httpCode > 0)
         { //Check for the returning code
 
             String payload = http.getString();
-            Serial.println(httpCode);
             Serial.println(payload);
+
+            // move servo according to payload
+            if (payload == "rainy")
+            {
+                // Do Rainy Stuff
+                servo.write(0);
+            }
+            else if (payload == "cloudy")
+            {
+                // Do Cloudy Stuff
+                servo.write(90);
+            }
+            else
+            {
+                // Assume Clear
+                servo.write(180);
+            }
         }
 
         else
         {
+            Serial.println(httpCode);
             Serial.println("Error on HTTP request");
         }
 
         http.end(); //Free the resources
     }
 
-    delay(1000000000);
+    delay(600000); // wait 10 minutes, check in
 }
