4f5e562e5d80c7da487abcf9e984e6cd3332f5ae
cyko28 <scott.t.robertson@gmail.com>
2018-05-16 20:41:37 -0400

    filter the results to return weather condition for esp32

--- nodejs/darksky.js
+++ nodejs/darksky.js
@@ -7,6 +7,29 @@ const port = process.env.PORT || 8080;
 const DarkSky = require('dark-sky');
 const darksky = new DarkSky(process.env.DARK_SKY); // Your API KEY can be hardcoded, but I recommend setting it as an env variable.
 
+const weatherMap = [
+    {
+        name: 'clear',
+        conditions: ['clear-day', 'clear-night', 'wind']
+    },
+    {
+        name: 'cloudy',
+        conditions: [
+            'fog',
+            'cloudy',
+            'partly-cloudy-day',
+            'partly-cloudy-night'
+        ]
+    },
+    {
+        name: 'rainy',
+        conditions: ['rain', 'snow', 'sleet', 'hail']
+    }
+];
+
+const getWeather = condition =>
+    weatherMap.find(weather => weather.conditions.includes(condition)).name;
+
 app.listen(port, function() {
     console.log('Server is running on ' + port + ' port');
 });
@@ -16,15 +39,22 @@ app.get('/', function(req, res) {
 });
 
 app.get('/ds', function(req, res) {
-    darksky
-        .options({
-            latitude: process.env.LAT,
-            longitude: process.env.LON,
-            language: 'en',
-            exclude: ['currently', 'minutely', 'daily', 'alerts', 'flags']
-        })
-        .get()
-        .then(function(data) {
-            res.send('json is: ' + data.hourly.icon);
-        });
+    if (req.query.key === process.env.SECRET_KEY) {
+        darksky
+            .options({
+                latitude: process.env.LAT,
+                longitude: process.env.LON,
+                language: 'en',
+                exclude: ['currently', 'minutely', 'daily', 'alerts', 'flags']
+            })
+            .get()
+            .then(function(data) {
+                let weatherType = data.hourly.icon;
+                res.send(getWeather(weatherType));
+            });
+    } else {
+        res.send('Bad Key');
+    }
+    console.log(req.query.key);
+    console.log(process.env.SECRET_KEY);
 });
